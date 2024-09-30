4388f3b3ff4f0432e062a88bf66e34338db95700
olgaeger <pavolha@mail.ru>
2024-09-30 22:59:11 +0400

    add files

---
+++ lesson 2.1/app.js
@@ -0,0 +1,10 @@
+//const firstName = 'Olga'
+//firstName = 'Ivan'
+//console.log('Hello', firstName)
+
+let firstName = 'Olga'
+let age = '34'
+let city = 'Batumi'
+let hobbi = 'art'
+
+console.log('Hello', firstName)
