b890897ea953dac945338db8cc28a8f45ef8e4fb
olgaeger <pavolha@mail.ru>
2024-10-07 18:17:48 +0400

    add lesson2.3

--- lesson 2.2/lesson 2.2.js
+++
@@ -1,8 +0,0 @@
-let titleProject = 'Оценка стоимости'
-let screensValue = 'шаблонные, с уникальным дизайном, с анимациями'
-let screenPrice = 5000
-let percentage = 30
-let fullPrice = 35000
-let responsive = true
-
-console.log(typeof titleProject)
