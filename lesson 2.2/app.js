3420ab37e3f0c3b967f0f9223262c0b7bbda28cc
olgaeger <pavolha@mail.ru>
2024-10-07 17:13:10 +0400

    add lesson 2.2

---
+++ lesson 2.2/app.js
@@ -0,0 +1,6 @@
+const titleProject = 'Оценка стоимости'
+const screensValue = 'шаблонные, с уникальным дизайном, с анимациями'
+const screenPrice = 5000
+const percentage = 30
+const fullPrice = 35000
+const responsive = true
