d0814807046fea0d68b04b64ae6d9be70b5158b5
olgaeger <pavolha@mail.ru>
2024-10-07 17:29:08 +0400

    self-check

--- lesson 2.2/app.js
+++
@@ -1,6 +0,0 @@
-const titleProject = 'Оценка стоимости'
-const screensValue = 'шаблонные, с уникальным дизайном, с анимациями'
-const screenPrice = 5000
-const percentage = 30
-const fullPrice = 35000
-const responsive = true
---
+++ lesson 2.2/lesson 2.2.js
@@ -0,0 +1,6 @@
+let titleProject = 'Оценка стоимости'
+let screensValue = 'шаблонные, с уникальным дизайном, с анимациями'
+let screenPrice = 5000
+let percentage = 30
+let fullPrice = 35000
+let responsive = true
