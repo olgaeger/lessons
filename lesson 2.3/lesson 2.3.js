721e14fa927df4f001ba51c82a0d7bccf2eb760e
olgaeger <pavolha@mail.ru>
2024-10-09 17:04:58 +0400

    add Lesson2.3

---
+++ lesson 2.3/lesson 2.3.js
@@ -0,0 +1,31 @@
+let titleProject = prompt('Название проекта?');
+let screensValue = prompt('Какие типы экранов потребуются: шаблонные, с уникальным дизайном, с анимациями');
+let responsive = prompt('Нужен ли респонсивный сайт?');
+if (responsive = 'да') {
+    responsive = true;
+} else {
+    responsive = false;
+}
+
+let service1 = prompt('Какой сервис нужен?');
+let servicePrice1 = prompt('Сколько это будет стоить?');
+let service2 = prompt('Какой еще сервис тебе нужен?');
+let servicePrice2 = prompt('Сколько будет стоить этот второй сервис?');
+
+let screenPrice = 5000;
+let fullPrice = screenPrice + servicePrice1 + servicePrice2;
+if (fullPrice > 50000) {
+    console.log('сделаем скидку в 10%');
+} else if (20000 < fullPrice <= 50000) {
+    console.log('сделаем скидку 5%');
+} else if (0 < fullPrice <= 20000) {
+    console.log('скидка не предусмотрена');
+} else if (fullPrice < 0) {
+    console.log('Что то пошло не так');
+}
+
+let percentage = fullPrice * 0.15;
+let servicePercentPrice = fullPrice - percentage;
+servicePercentPrice = Math.round(servicePercentPrice);
+
+console.log(servicePercentPrice);
