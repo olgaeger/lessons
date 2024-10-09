c124b766b8329212b6a732c4f7f9c141df800bee
olgaeger <pavolha@mail.ru>
2024-10-09 17:52:43 +0400

    check from video

--- lesson 2.3/lesson 2.3.js
+++ lesson 2.3/lesson 2.3.js
@@ -1,32 +1,41 @@
+let screenPrice = 5000;
+let percentage = 15;
+
 let titleProject = prompt('Название проекта?');
+console.log(titleProject);
+
 let screensValue = prompt('Какие типы экранов потребуются: шаблонные, с уникальным дизайном, с анимациями');
+console.log(screensValue);
+
 let responsive = prompt('Нужен ли респонсивный сайт?');
-if (responsive = 'да') {
-    responsive = true;
-} else {
-    responsive = false;
-}
+console.log(responsive);
 
 let service1 = prompt('Какой сервис нужен?');
-let servicePrice1 = prompt('Сколько это будет стоить?');
-let service2 = prompt('Какой еще сервис тебе нужен?');
-let servicePrice2 = prompt('Сколько будет стоить этот второй сервис?');
+console.log(service1);
 
-let screenPrice = 5000;
-let fullPrice = +screenPrice + +servicePrice1 + +servicePrice2;
+let servicePrice1 = +prompt('Сколько это будет стоить?');
+console.log(servicePrice1);
+
+let service2 = prompt('Какой еще сервис тебе нужен?');
+console.log(service2);
+
+let servicePrice2 = +prompt('Сколько будет стоить этот второй сервис?');
+console.log(servicePrice2);
+
+let fullPrice = screenPrice + servicePrice1 + servicePrice2;
+
+let percentageResult = +(fullPrice * (percentage / 100));
+console.log(percentageResult, 'Процент подрядчику');
+
+let servicePercentPrice = fullPrice - percentageResult;
+console.log(Math.ceil(servicePercentPrice), 'Итоговая сумма проекта за вычетом % подрядчику');
+
 if (fullPrice > 50000) {
     console.log('сделаем скидку в 10%');
 } else if (20000 < fullPrice <= 50000) {
     console.log('сделаем скидку 5%');
-} else if (0 < fullPrice <= 20000) {
+} else if (0 <= fullPrice <= 20000) {
     console.log('скидка не предусмотрена');
-} else if (fullPrice <= 0) {
+} else if (fullPrice < 0) {
     console.log('Что то пошло не так');
 }
-
-let percentage = 15;
-let percentageResult = fullPrice - percentage/100;
-let servicePercentPrice = fullPrice - percentageResult;
-servicePercentPrice = Math.round(servicePercentPrice);
-
-console.log(servicePercentPrice);
